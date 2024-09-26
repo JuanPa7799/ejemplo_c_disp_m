7cf143b54fd129fa35ea31a65cd93c574744fdf8
JuanPa <garsa779999@gmail.com>
2024-09-26 08:59:42 -0600

    Example ok

---
+++ main.c
@@ -0,0 +1,10 @@
+// documentación ----
+//
+// Juan Pablo García Chávez
+#include <stdio.h>
+int main (int argc, char * argv[]){ /*Es un contador de argumentos el primero y el segundo es un arreglo donde se guardan cada uno de los argumentos.*/
+	printf("Hello World from BBB!!\n");
+	return 0;
+}
+
+
